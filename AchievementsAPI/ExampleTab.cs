85aa24194cc3833d7e7f1295bb9b6b58aad0fdbe
wanderingpix <wanderingpixel0@gmail.com>
2026-06-01 21:34:38 +0100

    Tab Colors, and fix sprite loading from external assemblies.

--- AchievementsAPI/ExampleTab.cs
+++ AchievementsAPI/ExampleTab.cs
@@ -7,6 +7,11 @@ namespace AchievementsAPI;
 public class ExampleTab : AchievementsTab
 {
     public override string Name => "Example Tab OwO";
+    public override Color GetTabColor()
+    {
+        return Color.red;
+    }
+
     public BaseAchievement achievement { get; set; } = new BaseAchievement("Double Double Trouble", "uwu", "AchievementsAPI.Resources.ExampleIcon.png");
     public CountAchievement achievement2 { get; set; } = new CountAchievement("Skibidi", ":3", "AchievementsAPI.Resources.ExampleIcon.png", 15, 20, true);
 
