5aa51646a3f983f9aeeb2b658c9cd4d12d38615e
am-clonec <s.brouwer111@gmail.com>
2026-06-02 10:08:00 +0200

    made button to access achievements screen!!

---
+++ AchievementsAPI/Patches/MainMenu/AchievementsMenuOpen.cs
@@ -0,0 +1,14 @@
+﻿using AchievementsAPI;
+using UnityEngine;
+
+namespace AchievementsAPI.MainMenu;
+
+public class AchievementsMenuOpen
+{
+    public static void OpenMenu()
+    {
+
+        var menu = Object.Instantiate(Assets.achievementPrefab);
+        menu.SetActive(true);
+    }
+}
