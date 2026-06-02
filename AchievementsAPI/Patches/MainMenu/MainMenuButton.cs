12990304cee83eb6ab8815526a4082313f0a8aa5
wanderingpix <wanderingpixel0@gmail.com>
2026-06-02 15:06:06 +0100

    new button sprites

--- AchievementsAPI/Patches/MainMenu/MainMenuButton.cs
+++ AchievementsAPI/Patches/MainMenu/MainMenuButton.cs
@@ -10,6 +10,7 @@ using Reactor.Utilities.Extensions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Action = Il2CppSystem.Action;
 using Debug = Il2CppSystem.Diagnostics.Debug;
 using Object = UnityEngine.Object;
 
@@ -21,12 +22,15 @@ public static class MainMenuButtons
 
     public static void SetUp(MainMenuManager menu)
     {
-        var sprit = SpriteTools.LoadSpriteFromPath("AchievementsAPI.Resources.ExampleIcon.png", Assembly.GetCallingAssembly(), 100);
-        var sprit2 = SpriteTools.LoadSpriteFromPath("AchievementsAPI.Resources.ExampleIconSecond.png", Assembly.GetCallingAssembly(), 100);
-        button = PassiveButtonStuff.CreatePassiveButton("AchievementsMenu", sprit2, sprit, new Vector2(1f, 1f), () => AchievementsMenuOpen.OpenMenu(menu));
+        var sprit = SpriteTools.LoadSpriteFromPath("AchievementsAPI.Resources.AchievementsButtonActive.png", Assembly.GetCallingAssembly(), 100);
+        var sprit2 = SpriteTools.LoadSpriteFromPath("AchievementsAPI.Resources.AchievementsButton.png", Assembly.GetCallingAssembly(), 100);
+        button = PassiveButtonStuff.CreatePassiveButton("AchievementsMenuButton", sprit2, sprit, new Vector2(1f, 1f), () => AchievementsMenuOpen.OpenMenu(menu));
+        menu.StartCoroutine(Effects.ActionAfterDelay(0.1f, new System.Action(() =>
+        {
+            button.activeSprites.GetComponent<SpriteRenderer>().color = Color.white;
+            button.inactiveSprites.GetComponent<SpriteRenderer>().color = Color.white;
+        })));
         button.transform.SetParent(menu.transform.FindChild("MainUI/AspectScaler"));
-        button.transform.localPosition = new Vector3(5.6f, 1.55f, 0);
+        button.transform.localPosition = new Vector3(5.6f, 1.55f, 5);
     }
-    
-    
 }
