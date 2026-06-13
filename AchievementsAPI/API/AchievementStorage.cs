25c9d6e03c4c9c3d7825bf0272a8931910512761
am-clonec <s.brouwer111@gmail.com>
2026-06-13 08:55:42 +0200

    Fixed storage on Starlight as we apparently had the wrong path. (Thanks Robin for informing me of this) Also made it so it's actually a path to a json instead of just to sl_data_path (which was incorrect btw)

--- AchievementsAPI/API/AchievementStorage.cs
+++ AchievementsAPI/API/AchievementStorage.cs
@@ -21,7 +21,7 @@ public class AchievementData
 public class AchievementStorage
 {
     public static List<AchievementData> BaseAchievements = new List<AchievementData>();
-    public static string JsonPath => OperatingSystem.IsAndroid() ? Environment.GetEnvironmentVariable("SL_DATA_PATH") : Path.Combine(Application.persistentDataPath, "AchievementsAPIData/achievements.json");
+    public static string JsonPath => OperatingSystem.IsAndroid() ? Path.Combine(Environment.GetEnvironmentVariable("STAR_DATA_PATH"), "AchievementsAPIData/achievements.json") : Path.Combine(Application.persistentDataPath, "AchievementsAPIData/achievements.json");
     
     public static void AchievementStorageUpdate(BaseAchievement achievement, bool unlocked)
     {
